5d29d3eb620da9891d7231b22228e917b699b78e
facelessuser <faceless.shop@gmail.com>
2011-09-16 12:52:30 -0600

    Edited SublimeBrackets.py via GitHub

--- SublimeBrackets.py
+++ SublimeBrackets.py
@@ -1,43 +1,28 @@
 import sublime, sublime_plugin
 
 class SublimeBrackets(sublime_plugin.EventListener):
-
-  # Customize
-  #--------------------
-  # Scope? (Defined in theme files.) ->
-  # Examples: (keyword/string/number)
-  CurlyScope = 'entity.name.class'
-  RoundScope = 'entity.name.class'
-  SquareScope = 'entity.name.class'
-  # Outline? (True/False) ->
-  CurlyOutline = True
-  RoundOutline = True
-  SquareOutline = True
-  # Icon? (dot/circle/bookmark/cross)
-  CurlyIcon = 'dot'
-  RoundIcon = 'dot'
-  SquareIcon = 'dot'
-  #--------------------
-  # End Customize
-
+  #Initialize
+  Settings = sublime.load_settings('SublimeBrackets.sublime-settings')
+  Targets = None
+  highlight_us = None
+  SearchThreshold = None
   view = None
   window = None
   last_id_view = None
   last_id_sel = None
-  highlight_us = None
 
   def on_selection_modified(self, view):
     self.view = view
     self.window = view.window()
     self.last_view = view
 
+    self.init()
+
     if(self.unique()):
       # Clear views.
-      self.highlight_us = {
-        'curly': [],
-        'round': [],
-        'square': []
-      }
+      for bracket in self.highlight_us:
+        self.highlight_us[bracket] = []
+
       if self.window != None:
         for clear_view in self.window.views():
           self.highlight(clear_view)
@@ -46,6 +31,27 @@ class SublimeBrackets(sublime_plugin.EventListener):
         self.match_braces(sel)
     # Highlight.
     self.highlight(view)
+
+  def init(self):
+    self.SearchThreshold = int(self.Settings.get('search_threshold'))
+    self.Targets = {}
+    self.highlight_us = {}
+    self.addBracket('curly','{','}')
+    self.addBracket('round','(',')')
+    self.addBracket('square','[',']')
+    self.addBracket('angle','<','>')
+    print self.Targets
+
+  def addBracket(self, bracket, opening, closing):
+    if(bool(self.Settings.get(bracket+'_enable')) == True):
+      self.Targets[bracket] = {
+        'scope' : self.Settings.get(bracket+'_scope'),
+        'outline' : bool(self.Settings.get(bracket+'_outline')),
+        'icon' : self.Settings.get(bracket+'_icon'),
+        'open' : opening,
+        'close' : closing
+      }
+      self.highlight_us[bracket] = []
 
   def unique(self):
     id_view = self.view.id()
@@ -61,76 +67,33 @@ class SublimeBrackets(sublime_plugin.EventListener):
       return False
 
   def highlight(self, view):
-
-    # Curly Highlight.
-    if(self.CurlyOutline == True):
-      view.add_regions(
-        'curly', 
-        self.highlight_us['curly'], 
-        self.CurlyScope,
-        self.CurlyIcon,
-        sublime.DRAW_OUTLINED
-      )
-    elif(self.CurlyOutline == False):
-      view.add_regions(
-        'curly', 
-        self.highlight_us['curly'], 
-        self.CurlyScope,
-        self.CurlyIcon,
-        sublime.HIDE_ON_MINIMAP
-      )
-
-    # Round Highlight.
-    if(self.RoundOutline == True):
-      view.add_regions(
-        'round', 
-        self.highlight_us['round'], 
-        self.RoundScope,
-        self.RoundIcon,
-        sublime.DRAW_OUTLINED
-      )
-    elif(self.RoundOutline == False):
-      view.add_regions(
-        'round', 
-        self.highlight_us['round'], 
-        self.RoundScope,
-        self.RoundIcon,
-        sublime.HIDE_ON_MINIMAP
-      )
-
-    # Square Highlight.
-    if(self.SquareOutline == True):
-      view.add_regions(
-        'square', 
-        self.highlight_us['square'], 
-        self.SquareScope,
-        self.SquareIcon,
-        sublime.DRAW_OUTLINED
-      )
-    elif(self.SquareOutline == False):
-      view.add_regions(
-        'square', 
-        self.highlight_us['square'], 
-        self.SquareScope,
-        self.SquareIcon,
-        sublime.HIDE_ON_MINIMAP
-      )
+    for bracket in self.Targets:
+      if(self.Targets[bracket]['outline'] == True):
+        view.add_regions(
+          bracket,
+          self.highlight_us[bracket],
+          self.Targets[bracket]['scope'],
+          self.Targets[bracket]['icon'],
+          sublime.DRAW_OUTLINED
+        )
+      elif(self.Targets[bracket]['outline'] == False):
+        view.add_regions(
+          bracket, 
+          self.highlight_us[bracket], 
+          self.Targets[bracket]['scope'],
+          self.Targets[bracket]['icon'],
+          sublime.HIDE_ON_MINIMAP
+        )
 
   def match_braces(self, sel):
     left = self.scout_left(sel.a)
     if(left != None):
-      if(self.view.substr(left) == '{'):
-        self.bracket_type = 'curly'
-        self.bracket_open = '{'
-        self.bracket_close = '}'
-      if(self.view.substr(left) == '('):
-        self.bracket_type = 'round'
-        self.bracket_open = '('
-        self.bracket_close = ')'
-      if(self.view.substr(left) == '['):
-        self.bracket_type = 'square'
-        self.bracket_open = '['
-        self.bracket_close = ']'
+      for bracket in self.Targets:
+        if(self.view.substr(left) == self.Targets[bracket]['open']):
+          self.bracket_type = bracket
+          self.bracket_open = self.Targets[bracket]['open']
+          self.bracket_close = self.Targets[bracket]['close']
+          break
       right = self.scout_right(left + 1)
     if(left != None and right != None):
       region = sublime.Region(left, left + 1)
@@ -139,31 +102,47 @@ class SublimeBrackets(sublime_plugin.EventListener):
       self.highlight_us[self.bracket_type].append(region)
 
   def scout_left(self, scout):
-    brackets = {
-      'curly': 0,
-      'round': 0,
-      'square': 0,
-    }
+    brackets = {}
+    for bracket in self.Targets:
+      brackets[bracket] = {
+        'count' : 0,
+        'open' : self.Targets[bracket]['open'],
+        'close' : self.Targets[bracket]['close'],
+      }
+
     a_check = True
     b_check = True
-    scout = scout + 1
+    scout += 1
+    max_search = 0
     while(scout > 0):
-      scout = scout - 1
+      max_search += 1
+      if (max_search >= self.SearchThreshold):
+        return None
+      scout -= 1
       # Cicumstance checks.
+      next = False
       if(a_check == True):
         a_check = False
         char = self.view.substr(scout - 1)
-        if( char == '}' or
-            char == ')' or
-            char == ']'):
-          continue
+        for bracket in brackets:
+          if(char == brackets[bracket]['close']):
+            next = True
+            break
+
+      if(next == True):
+        continue
+
       if(b_check == True):
         b_check = False
         char = self.view.substr(scout)
-        if( char == '}' or
-            char == ')' or
-            char == ']'):
-          continue
+        for bracket in brackets:
+          if(char == brackets[bracket]['close']):
+            next = True
+            break
+
+      if(next == True):
+        continue
+
       # Are we in a string?
       if( self.view.score_selector(scout, 'string') > 0 or
           self.view.score_selector(scout, 'comment') > 0): 
@@ -171,35 +150,35 @@ class SublimeBrackets(sublime_plugin.EventListener):
       # Assign char.
       char = self.view.substr(scout)
       # Hit brackets.
-      if(char == '{'):
-        if(brackets['curly'] > 0):
-          brackets['curly'] = brackets['curly'] - 1
-        else: 
-          return scout
-      elif(char == '('):
-        if(brackets['round'] > 0):
-          brackets['round'] = brackets['round'] - 1
-        else: 
-          return scout
-      elif(char == '['):
-        if(brackets['square'] > 0):
-          brackets['square'] = brackets['square'] - 1
-        else: 
-          return scout
-      elif(char == '}'):
-        brackets['curly'] = brackets['curly'] + 1
-      elif(char == ')'):
-        brackets['round'] = brackets['round'] + 1
-      elif(char == ']'):
-        brackets['square'] = brackets['square'] + 1
+      for bracket in brackets:
+        if (char == brackets[bracket]['open']):
+          if(brackets[bracket]['count'] > 0):
+            brackets[bracket]['count'] -= 1
+            next == True
+            break
+          else:
+            return scout
+
+      if(next == True):
+        continue
+
+      for bracket in brackets:
+        if (char == brackets[bracket]['close']):
+          brackets[bracket]['count'] += 1
+          break
+
 
   def scout_right(self, scout):
     brackets = {
       'parentheses': 0
     }
-    scout = scout - 1
+    scout -= 1
+    max_search = 0
     while(scout < self.view.size()):
-      scout = scout + 1
+      max_search += 1
+      if (max_search >= self.SearchThreshold):
+        return None
+      scout += 1
       # Are we in a string?
       if( self.view.score_selector(scout, 'string') > 0 or
           self.view.score_selector(scout, 'comment') > 0): 
@@ -209,8 +188,8 @@ class SublimeBrackets(sublime_plugin.EventListener):
       # Hit brackets.
       if(char == self.bracket_close):
         if(brackets['parentheses'] > 0):
-          brackets['parentheses'] = brackets['parentheses'] - 1
+          brackets['parentheses'] -= 1
         else: 
           return scout
       elif(char == self.bracket_open):
-        brackets['parentheses'] = brackets['parentheses'] + 1
+        brackets['parentheses'] += 1
