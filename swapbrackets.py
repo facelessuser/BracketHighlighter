de2ce0e1fb813906f7a0127dae52da90a3a474b3
Isaac <faceless.shop@gmail.com>
2011-11-09 23:32:27 -0700

    Multi-select for Bracket Swap / Add Fold by Brackets Plugin
    -Adjust Bracket Swap to allow multi-select
    -Add Fold by Brackets plugin as a work around until ST2 properly
    supports folding by brackets

--- swapbrackets.py
+++ swapbrackets.py
@@ -1,26 +1,42 @@
 import bracket_plugin
 import sublime
+import sublime_plugin
 
 
-class swap_brackets(bracket_plugin.BracketPluginCommand):
-    brackets = None
-
-    def swap_brackets(self, value):
-        brackets = [
-            ["[", "]"],
-            ["(", ")"],
-            ["<", ">"],
-            ["{", "}"]
-        ]
+class swap_brackets_transform(bracket_plugin.BracketPluginCommand):
+    def run(self, bracket, content, selection, bracket_type="square"):
+        brackets = {
+            "square": ["[", "]"],
+            "round": ["(", ")"],
+            "angle": ["<", ">"],
+            "curly": ["{", "}"]
+        }
         edit = self.view.begin_edit()
-        self.view.replace(edit, sublime.Region(self.brackets.begin(), self.brackets.begin() + 1), brackets[value][0])
-        self.view.replace(edit, sublime.Region(self.brackets.end() - 1, self.brackets.end()), brackets[value][1])
+        self.view.replace(edit, sublime.Region(bracket.begin(), bracket.begin() + 1), brackets[bracket_type][0])
+        self.view.replace(edit, sublime.Region(bracket.end() - 1, bracket.end()), brackets[bracket_type][1])
         self.view.end_edit(edit)
 
-    def run(self, bracket, content, selection):
-        self.brackets = bracket
 
-        self.view.window().show_quick_panel(
+class SwapBracketsCommand(sublime_plugin.WindowCommand):
+    def swap_brackets(self, value):
+        brackets = [
+            "square",
+            "round",
+            "angle",
+            "curly"
+        ]
+        self.window.run_command(
+            "bracket_highlighter_key", {
+                "plugin": {
+                    "type": ["bracket"],
+                    "command": "swapbrackets.swap_brackets_transform",
+                    "args": {"bracket_type": brackets[value]}
+                }
+            }
+        )
+
+    def run(self):
+        self.window.show_quick_panel(
             [
                 "[] square", "() round",
                 "<> angle", "{} curly"
