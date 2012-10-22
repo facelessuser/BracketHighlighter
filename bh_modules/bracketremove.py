cd28d9d8d87226189b2a06a812ea89c0a0181512
facelessuser <faceless.shop@gmail.com>
2012-10-21 20:36:23 -0600

    -Much better and more flexible bracket remove

--- bh_modules/bracketremove.py
+++ bh_modules/bracketremove.py
@@ -1,5 +1,6 @@
 import bh_plugin
 import re
+import sublime
 
 
 class BracketRemove(bh_plugin.BracketPluginCommand):
@@ -14,13 +15,23 @@ class BracketRemove(bh_plugin.BracketPluginCommand):
                 if m:
                     self.view.replace(edit, line, m.group(1) + m.group(2))
 
-    def run(self, edit, name, remove_indent=False):
-        row_first = self.view.rowcol(self.left.end)[0] + 1
-        row_last = self.view.rowcol(self.right.begin)[0] - 1
-        self.view.replace(edit, self.right.toregion(), "")
-        if remove_indent:
-            self.decrease_indent_level(edit, row_first, row_last)
-        self.view.replace(edit, self.left.toregion(), "")
+    def run(self, edit, name, remove_content=False, remove_indent=False, remove_block=False):
+        print "here"
+        if remove_content:
+            self.view.replace(edit, sublime.Region(self.left.begin, self.right.end), "")
+        else:
+            row_first = self.view.rowcol(self.left.end)[0] + 1
+            row_last = self.view.rowcol(self.right.begin)[0] - 1
+            if remove_block:
+                self.view.replace(edit, self.view.full_line(self.right.toregion()), "")
+            else:
+                self.view.replace(edit, self.right.toregion(), "")
+            if remove_indent:
+                self.decrease_indent_level(edit, row_first, row_last)
+            if remove_block:
+                self.view.replace(edit, self.view.full_line(self.left.toregion()), "")
+            else:
+                self.view.replace(edit, self.left.toregion(), "")
 
         self.left = None
         self.right = None
---
+++ bh_remove.py
@@ -0,0 +1,39 @@
+import sublime_plugin
+from collections import namedtuple
+
+MENU = namedtuple("Menu", "simple content block block_indent")(
+    "Remove Brackets",
+    "Remove Brackets and Content",
+    "Remove Brackets: Block",
+    "Remove Brackets: Indented Block"
+)
+
+
+class BhRemoveBracketsCommand(sublime_plugin.WindowCommand):
+    def remove_brackets(self, value):
+        if value != -1:
+            menu_item = MENU[value]
+            indent = menu_item == MENU.block_indent
+            block = menu_item == MENU.block or menu_item == MENU.block_indent
+            content = menu_item == MENU.content
+
+            self.window.run_command(
+                "bh_key",
+                {
+                    "plugin": {
+                        "type": ["__all__"],
+                        "command": "bh_modules.bracketremove",
+                        "args": {
+                            "remove_indent": indent,
+                            "remove_block": block,
+                            "remove_content": content
+                        }
+                    }
+                }
+            )
+
+    def run(self):
+        self.window.show_quick_panel(
+            list(MENU),
+            self.remove_brackets
+        )
