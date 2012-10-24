e491ce2982be9449037f6010254447c5519d2497
facelessuser <faceless.shop@gmail.com>
2012-10-23 19:05:20 -0600

    -Better ruby support

---
+++ bh_modules/rubykeywords.py
@@ -0,0 +1,12 @@
+import re
+
+
+def post_match(view, name, first, second, center, bfr, threshold):
+    if first is not None:
+        # Strip whitespace from the beginning of first bracket
+        open_bracket = bfr[first.begin:first.end]
+        if open_bracket != "do":
+            m = re.match(r"^(\s*\b)[\w\W]*", open_bracket)
+            if m:
+                first = first.move(first.begin + m.end(1), first.end)
+    return first, second, name
