8328cf9ecf3886a7d6fa01cd1e7d3b30e0a22965
Isaac <muse_i@MuseI-Opt990.wdl.wdc.com>
2013-04-29 12:47:06 -0600

    -Fix issue where conditional statements in HTML (ruby) would be matched in HTML text -Fix weird windows Regex issue with <%

--- bh_modules/rubykeywords.py
+++ bh_modules/rubykeywords.py
@@ -6,7 +6,7 @@ def post_match(view, name, style, first, second, center, bfr, threshold):
         # Strip whitespace from the beginning of first bracket
         open_bracket = bfr[first.begin:first.end]
         if open_bracket != "do":
-            m = re.match(r"(<%\s*|^\s*\b)[\w\W]*", open_bracket)
+            m = re.match(r"(%\s*|^\s*\b)[\w\W]*", open_bracket)
             if m:
                 first = first.move(first.begin + m.end(1), first.end)
     return first, second, style
