77e1632fa508a6234c25eebc0ecf48a67d42f56d
facelessuser <faceless.shop@gmail.com>
2013-04-28 21:30:08 -0600

    -Simplify ruby keyword logic

--- bh_modules/rubykeywords.py
+++ bh_modules/rubykeywords.py
@@ -6,11 +6,7 @@ def post_match(view, name, style, first, second, center, bfr, threshold):
         # Strip whitespace from the beginning of first bracket
         open_bracket = bfr[first.begin:first.end]
         if open_bracket != "do":
-            m = re.match(r"^(\s*\b)[\w\W]*", open_bracket)
-            if m:
-                first = first.move(first.begin + m.end(1), first.end)
-        if name == "ruby_embedded_html":
-            m = re.match(r"(<%\s*)[\w\W]*", open_bracket)
+            m = re.match(r"(<%\s*|^\s*\b)[\w\W]*", open_bracket)
             if m:
                 first = first.move(first.begin + m.end(1), first.end)
     return first, second, style
