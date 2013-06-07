ecb2bb609974e51ae36d361cddc9f19a5013b818
facelessuser <faceless.shop@gmail.com>
2013-06-07 08:32:46 -0600

    -Fix swap brackets in strings

--- bh_modules/swapbrackets.py
+++ bh_modules/swapbrackets.py
@@ -6,8 +6,14 @@ BracketRemove = ImpMod.import_from("bh_modules.bracketremove", "BracketRemove")
 class SwapBrackets(BracketRemove):
     def run(self, edit, name, remove_content=False, remove_indent=False, remove_block=False):
         offset = self.left.toregion().size()
-        self.selection = [sublime.Region(self.left.begin, self.right.begin - offset)]
+        selection = [sublime.Region(self.left.begin, self.right.begin - offset)]
+        left = self.left.move(self.left.end, self.left.end)
+        right = self.right.move(self.right.begin, self.right.begin)
         super(SwapBrackets, self).run(edit, name)
+        self.selection = selection
+        self.left = left
+        self.right = right
+        self.nobracket = False
 
 
 def plugin():
