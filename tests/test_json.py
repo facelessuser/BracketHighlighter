2b17dc6d75af36709e945787f0403d8c03a6e858
facelessuser <faceless.shop@gmail.com>
2015-05-13 21:27:22 -0600

    JSON test now searches for all JSON files

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,25 +1,36 @@
 """Test JSON."""
 import unittest
 from . import validate_json_format
+import os
+import fnmatch
 
 
 class TestSettings(unittest.TestCase):
 
     """Test JSON settings."""
 
+    def _get_json_files(self, pattern):
+        """Get json files."""
+
+        for root, dirnames, filenames in os.walk('.'):
+            for filename in fnmatch.filter(filenames, pattern):
+                yield os.path.join(root, filename)
+
     def test_json_settings(self):
         """Test each JSON file."""
 
-        files = [
-            "bh_swapping.sublime-settings",
-            "bh_wrapping.sublime-settings",
-            "Default.sublime-keymap",
-            "Default.sublime-commands",
-            "bh_core.sublime-settings"
-        ]
+        patterns = (
+            '*.sublime-settings',
+            '*.sublime-keymap',
+            '*.sublime-commands',
+            '*.sublime-menu',
+            '*.sublime-theme'
+        )
 
-        for f in files:
-            self.assertFalse(
-                validate_json_format.CheckJsonFormat(False, True).check_format(f),
-                "%s does not comform to expected format!" % f
-            )
+        for pattern in patterns:
+            for f in self._get_json_files(pattern):
+                print(f)
+                self.assertFalse(
+                    validate_json_format.CheckJsonFormat(False, True).check_format(f),
+                    "%s does not comform to expected format!" % f
+                )
