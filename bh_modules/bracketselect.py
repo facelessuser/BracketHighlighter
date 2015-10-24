971d3dc73218fb32b7f1ab2a68dbdb877d9f1140
facelessuser <faceless.shop@gmail.com>
2015-10-24 10:27:03 -0600

    Reduce complexity in bracketselect.py

--- bh_modules/bracketselect.py
+++ bh_modules/bracketselect.py
@@ -22,65 +22,82 @@ class SelectBracket(bh_plugin.BracketPluginCommand):
         parent.
         """
 
-        if tags is None:
-            tags = DEFAULT_TAGS
-        current_left, current_right = self.selection[0].begin(), self.selection[0].end()
-        left, right = self.left, self.right
-        first, last = left.end, right.begin
+        self.tags = DEFAULT_TAGS if tags is None else tags
+        self.current_left, self.current_right = self.selection[0].begin(), self.selection[0].end()
+        first, last = self.left.end, self.right.begin
+
         if select == 'left':
-            if name in tags and left.size() > 1:
-                first, last = left.begin + 1, left.begin + 1
-                if first == current_left and last == current_right:
-                    self.refresh_match = True
-                    if alternate:
-                        first, last = right.begin + 1, right.begin + 1
-                    else:
-                        first, last = left.begin, left.begin
-            else:
-                first, last = left.end, left.end
-                if first == current_left and last == current_right:
-                    self.refresh_match = True
-                    if alternate:
-                        first, last = right.begin, right.begin
-                    else:
-                        first, last = left.begin, left.begin
+            first, last = self.select_left(name, first, last, alternate)
         elif select == 'right':
-            if left.end != right.end:
-                if name in tags and left.size() > 1:
-                    first, last = right.begin + 1, right.begin + 1
-                    if first == current_left and last == current_right:
-                        self.refresh_match = True
-                        if alternate:
-                            first, last = left.begin + 1, left.begin + 1
-                        else:
-                            first, last = right.end, right.end
-                else:
-                    first, last = right.begin, right.begin
-                    if first == current_left and last == current_right:
-                        self.refresh_match = True
-                        if alternate:
-                            first, last = left.end, left.end
-                        else:
-                            first, last = right.end, right.end
-            else:
-                # There is no second bracket, so just select the first
-                if name in tags and left.size() > 1:
-                    first, last = left.begin + 1, left.begin + 1
-                    if first == current_left and last == current_right:
-                        self.refresh_match = True
-                        if not alternate:
-                            first, last = left.end, left.end
-                else:
-                    first, last = right.end, right.end
-                    if first == current_left and last == current_right:
-                        self.refresh_match = True
-                        if not alternate:
-                            first, last = right.end, right.end
-        elif first == current_left and last == current_right or always_include_brackets:
-            first, last = left.begin, right.end
-            self.refresh_match = True
+            first, last = self.select_right(name, first, last, alternate)
+        elif first == self.current_left and last == self.current_right or always_include_brackets:
+            first, last = self.select_expand(first, last)
 
         self.selection = [sublime.Region(first, last)]
+
+    def select_left(self, name, first, last, alternate):
+        """Select the left bracket."""
+
+        if name in self.tags and self.left.size() > 1:
+            first, last = self.left.begin + 1, self.left.begin + 1
+            if first == self.current_left and last == self.current_right:
+                self.refresh_match = True
+                if alternate:
+                    first, last = self.right.begin + 1, self.right.begin + 1
+                else:
+                    first, last = self.left.begin, self.left.begin
+        else:
+            first, last = self.left.end, self.left.end
+            if first == self.current_left and last == self.current_right:
+                self.refresh_match = True
+                if alternate:
+                    first, last = self.right.begin, self.right.begin
+                else:
+                    first, last = self.left.begin, self.left.begin
+        return first, last
+
+    def select_right(self, name, first, last, alternate):
+        """Select the right bracket."""
+
+        if self.left.end != self.right.end:
+            if name in self.tags and self.left.size() > 1:
+                first, last = self.right.begin + 1, self.right.begin + 1
+                if first == self.current_left and last == self.current_right:
+                    self.refresh_match = True
+                    if alternate:
+                        first, last = self.left.begin + 1, self.left.begin + 1
+                    else:
+                        first, last = self.right.end, self.right.end
+            else:
+                first, last = self.right.begin, self.right.begin
+                if first == self.current_left and last == self.current_right:
+                    self.refresh_match = True
+                    if alternate:
+                        first, last = self.left.end, self.left.end
+                    else:
+                        first, last = self.right.end, self.right.end
+        else:
+            # Select the first because there is no second bracket.
+            if name in self.tags and self.left.size() > 1:
+                first, last = self.left.begin + 1, self.left.begin + 1
+                if first == self.current_left and last == self.current_right:
+                    self.refresh_match = True
+                    if not alternate:
+                        first, last = self.left.end, self.left.end
+            else:
+                first, last = self.right.end, self.right.end
+                if first == self.current_left and last == self.current_right:
+                    self.refresh_match = True
+                    if not alternate:
+                        first, last = self.right.end, self.right.end
+        return first, last
+
+    def select_expand(self, first, last):
+        """Expand content selection."""
+
+        first, last = self.left.begin, self.right.end
+        self.refresh_match = True
+        return first, last
 
 
 def plugin():
