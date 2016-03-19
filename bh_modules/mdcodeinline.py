95ae44f7beb8b54eca5cee15859746353f14b249
Richard Stein <r-stein.github@web.de>
2016-03-19 08:52:17 +0100

    Added module to improve inline markdown code

---
+++ bh_modules/mdcodeinline.py
@@ -0,0 +1,22 @@
+"""
+BracketHighlighter.
+
+Copyright (c) 2013 - 2015 Isaac Muse <isaacmuse@gmail.com>
+License: MIT
+"""
+
+
+def post_match(view, name, style, first, second, center, bfr, threshold):
+    """Ensure that backticks inside the inline are not highlighted."""
+    # ensure this is only used for inline markdown
+    if name != "mdcodeinline":
+        return first, second, style
+
+    inline_content_scope = "markup.raw.inline.content.markdown"
+    # move the first scope back to the left
+    if first is not None:
+        end = first.end
+        while view.score_selector(end - 1, inline_content_scope):
+            end -= 1
+        first = first.move(first.begin, end)
+    return first, second, style
