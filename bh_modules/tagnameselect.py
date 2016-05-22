535bccf9de98e93bb476ae1f63288a7b6f88524a
facelessuser <faceless.shop@gmail.com>
2016-05-22 16:14:08 -0600

    Tagnameselect should use common regex in settings
    - Use tag name regex from settings file.

--- bh_modules/tagnameselect.py
+++ bh_modules/tagnameselect.py
@@ -4,7 +4,10 @@ BracketHighlighter.
 Copyright (c) 2013 - 2016 Isaac Muse <isaacmuse@gmail.com>
 License: MIT
 """
+import sublime
 import BracketHighlighter.bh_plugin as bh_plugin
+from BracketHighlighter.bh_plugin import import_module
+tags = import_module("bh_modules.tags")
 
 
 class TagNameSelect(bh_plugin.BracketPluginCommand):
@@ -14,7 +17,9 @@ class TagNameSelect(bh_plugin.BracketPluginCommand):
         """Select tag name."""
 
         if self.left.size() > 1:
-            tag_name = '[\w\:\.\-]+'
+            tag_settings = sublime.load_settings("bh_tag.sublime-settings")
+            tag_mode = tags.get_tag_mode(self.view, tag_settings.get("tag_mode", {}))
+            tag_name = tag_settings.get('tag_name')[tag_mode]
             region1 = self.view.find(tag_name, self.left.begin)
             region2 = self.view.find(tag_name, self.right.begin)
             self.selection = [region1, region2]
