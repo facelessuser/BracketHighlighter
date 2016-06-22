69ee69df6bfbdd3405aec64b7bca36881a727d73
facelessuser <faceless.shop@gmail.com>
2016-06-22 07:19:52 -0600

    Support Ruby 2.1 function prefix
    fixes #348

--- bh_modules/rubykeywords.py
+++ bh_modules/rubykeywords.py
@@ -6,6 +6,11 @@ License: MIT
 """
 import re
 
+RE_DEF = re.compile(r"\s*(?:(?:private|public|protected)\s+)?(def)(\s+.*)?")
+SPECIAL_KEYWORDs = ('do')
+NORMAL_KEYWORDS = ('for', 'until', 'unless', 'while', 'class', 'module', 'if', 'begin', 'case')
+
+
 
 def post_match(view, name, style, first, second, center, bfr, threshold):
     """Strip whitespace from being targeted with highlight."""
@@ -13,8 +18,10 @@ def post_match(view, name, style, first, second, center, bfr, threshold):
     if first is not None:
         # Strip whitespace from the beginning of first bracket
         open_bracket = bfr[first.begin:first.end]
-        if open_bracket != "do":
-            m = re.match(r"(\s*\b)[\w\W]*", open_bracket)
-            if m:
-                first = first.move(first.begin + m.end(1), first.end)
+        if open_bracket != SPECIAL_KEYWORDs:
+            open_bracket = open_bracket.strip()
+            if open_bracket not in NORMAL_KEYWORDS:
+                m = RE_DEF.match(open_bracket)
+                if m:
+                    first = first.move(first.begin + m.start(1), first.begin + m.end(1))
     return first, second, style
