635259ea9f365d705cc9eb7d5b3990842bb37876
facelessuser <faceless.shop@gmail.com>
2016-06-22 07:30:42 -0600

    Regression related to
    Related to #348

--- bh_modules/rubykeywords.py
+++ bh_modules/rubykeywords.py
@@ -6,7 +6,8 @@ License: MIT
 """
 import re
 
-RE_DEF = re.compile(r"\s*(?:(?:private|public|protected)\s+)?(def)(\s+.*)?")
+RE_DEF = re.compile(r"\s*(?:(?:private|public|protected)\s+)?(def).*?")
+RE_KEYWORD = re.compile(r"(\s*\b)[\w\W]*")
 SPECIAL_KEYWORDs = ('do')
 NORMAL_KEYWORDS = ('for', 'until', 'unless', 'while', 'class', 'module', 'if', 'begin', 'case')
 
@@ -19,9 +20,13 @@ def post_match(view, name, style, first, second, center, bfr, threshold):
         # Strip whitespace from the beginning of first bracket
         open_bracket = bfr[first.begin:first.end]
         if open_bracket != SPECIAL_KEYWORDs:
-            open_bracket = open_bracket.strip()
-            if open_bracket not in NORMAL_KEYWORDS:
+            open_bracket_stripped = open_bracket.strip()
+            if open_bracket_stripped not in NORMAL_KEYWORDS:
                 m = RE_DEF.match(open_bracket)
                 if m:
                     first = first.move(first.begin + m.start(1), first.begin + m.end(1))
+            else:
+                m = RE_KEYWORD.match(open_bracket)
+                if m:
+                    first = first.move(first.begin + m.end(1), first.end)
     return first, second, style
