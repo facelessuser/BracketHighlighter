6724cefc02017b8035a297f107d209afc086761e
DeathAxe <deathaxe@web.de>
2016-10-26 19:36:44 +0200

    Add bh_modules for matching of opening and closing tags for "CNC SINUMERIK 840D language support"

---
+++ bh_modules/s840d_hmi.py
@@ -0,0 +1,22 @@
+"""
+BracketHighlighter.
+
+Copyright (c) 2013 - 2016 Deathaxe <deathaxe82@gmail.com>
+License: MIT
+"""
+
+def compare(name, first, second, bfr):
+    """Ensure correct open is paired with correct close."""
+
+    o = bfr[first.begin:first.end].lower()
+    c = bfr[second.begin:second.end].lower()
+
+    match = False
+    # classes
+    if o in ["//a", "//b", "//g", "//m", "//s"] and c == "//end":
+        match = True
+    # methods
+    elif c == "end_" + o:
+        match = True
+    return match
+
