ee64c84d7cc24a6f440d57f01d62806c0929f035
facelessuser <faceless.shop@gmail.com>
2016-11-10 21:40:54 -0700

    Add quick start guide and doc and issue links

---
+++ messages/install.md
@@ -0,0 +1,4 @@
+# BracketHighlighter
+
+Welcome to BracketHighlighter!  For a quick start guide, please go to  
+`Preferences->Package Settings->BracketHighlighter->Quick Start Guide`.
--- support.py
+++ support.py
@@ -2,9 +2,22 @@
 import sublime
 import sublime_plugin
 import textwrap
+import webbrowser
+import base64
+import re
 
 __version__ = "2.22.1"
 __pc_name__ = 'BracketHighlighter'
+
+CSS = '''
+div.bracket-highlighter { padding: 20px; margin: 0; }
+.bracket-highlighter h1, .bracket-highlighter h2, .bracket-highlighter h3,
+.bracket-highlighter h4, .bracket-highlighter h5, .bracket-highlighter h6 {
+    {{'.string'|css}}
+}
+.bracket-highlighter blockquote { {{'.comment'|css}} }
+.bracket-highlighter a { text-decoration: none; }
+'''
 
 
 def list2string(obj):
@@ -97,3 +110,56 @@ class BracketHighlighterSupportInfoCommand(sublime_plugin.ApplicationCommand):
 
         sublime.message_dialog(msg + '\nInfo has been copied to the clipboard.')
         sublime.set_clipboard(msg)
+
+
+class BracketHighlighterOpenSite(sublime_plugin.ApplicationCommand):
+    """Open site links."""
+
+    def run(self, url):
+        """Open the url."""
+
+        webbrowser.open_new_tab(url)
+
+
+class BracketHighlighterDoc(sublime_plugin.WindowCommand):
+    """Open doc page."""
+
+    def on_navigate(self, href):
+        """Handle links."""
+
+        if href.startswith('sub://Packages'):
+            sublime.run_command('open_file', {"file": '${packages}' + href[14:]})
+        else:
+            webbrowser.open_new_tab(href)
+
+    def run(self, page):
+        """Open page."""
+
+        try:
+            import mdpopups
+            has_phantom_support = (mdpopups.version() >= (1, 10, 0)) and (int(sublime.version()) >= 3118)
+        except Exception:
+            has_phantom_support = False
+
+        if not has_phantom_support:
+            sublime.run_command('open_file', {"file": page})
+        else:
+            text = sublime.load_resource(page.replace('${packages}', 'Packages'))
+            view = self.window.new_file()
+            view.set_name('BracketHighlighter - Quick Start')
+            view.settings().set('gutter', False)
+            if has_phantom_support:
+                mdpopups.add_phantom(
+                    view,
+                    'quickstart',
+                    sublime.Region(0),
+                    text,
+                    sublime.LAYOUT_INLINE,
+                    css=CSS,
+                    wrapper_class="bracket-highlighter",
+                    on_navigate=self.on_navigate
+                )
+            else:
+                view.run_command('insert', {"characters": text})
+            view.set_read_only(True)
+            view.set_scratch(True)
