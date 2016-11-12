ab6719f1c7e7dc5ced545023f5efb3526f82014e
facelessuser <faceless.shop@gmail.com>
2016-11-11 19:55:49 -0700

    Support changes
    - Move changes into support to share and share CSS.
    - Adjust link logic

--- support.py
+++ support.py
@@ -3,6 +3,7 @@ import sublime
 import sublime_plugin
 import textwrap
 import webbrowser
+import re
 
 __version__ = "2.22.1"
 __pc_name__ = 'BracketHighlighter'
@@ -122,11 +123,13 @@ class BracketHighlighterOpenSite(sublime_plugin.ApplicationCommand):
 class BracketHighlighterDoc(sublime_plugin.WindowCommand):
     """Open doc page."""
 
+    re_pkgs = re.compile(r'^Packages')
+
     def on_navigate(self, href):
         """Handle links."""
 
         if href.startswith('sub://Packages'):
-            sublime.run_command('open_file', {"file": '${packages}' + href[14:]})
+            sublime.run_command('open_file', {"file": self.re_pkgs.sub('${packages}', href[6:])})
         else:
             webbrowser.open_new_tab(href)
 
@@ -162,3 +165,39 @@ class BracketHighlighterDoc(sublime_plugin.WindowCommand):
                 view.run_command('insert', {"characters": text})
             view.set_read_only(True)
             view.set_scratch(True)
+
+
+class BracketHighlighterChangesCommand(sublime_plugin.WindowCommand):
+    """Changelog command."""
+
+    def run(self):
+        """Show the changelog in a new view."""
+        try:
+            import mdpopups
+            has_phantom_support = (mdpopups.version() >= (1, 10, 0)) and (int(sublime.version()) >= 3118)
+        except Exception:
+            has_phantom_support = False
+
+        text = sublime.load_resource('Packages/BracketHighlighter/CHANGES.md')
+        view = self.window.new_file()
+        view.set_name('BracketHighlighter - Changelog')
+        view.settings().set('gutter', False)
+        if has_phantom_support:
+            mdpopups.add_phantom(
+                view,
+                'changelog',
+                sublime.Region(0),
+                text,
+                sublime.LAYOUT_INLINE,
+                wrapper_class="bracket-highlighter",
+                css=CSS,
+                on_navigate=self.on_navigate
+            )
+        else:
+            view.run_command('insert', {"characters": text})
+        view.set_read_only(True)
+        view.set_scratch(True)
+
+    def on_navigate(self, href):
+        """Open links."""
+        webbrowser.open_new_tab(href)
